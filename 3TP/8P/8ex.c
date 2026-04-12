8f8f772b3bf8d614c613343692ca1e5940f032d1
Facundo-Sebastian-Ferraris <facundo.ferraris@est.fi.uncoma.edu.ar>
2026-04-12 00:56:17 -0300

    agregado tp3 resuelto  ej 8

--- 3TP/8P/8ex.c
+++ 3TP/8P/8ex.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 #include <stdlib.h>
+#define X 50000		//	5E4
+#define Y 10000		//	E4
+							//	matriz de 5E8 Elementos
 
-#define X 50000
-#define Y 10000
-
-int main() {
+int solucionInicial() {
 	int i, j;
 	int **m;
 	m = malloc(X * sizeof(int*));
@@ -12,5 +13,30 @@ int main() {
 	for(j = 0; j < Y; j++)
 		for(i = 0; i < X; i++)
 			m[i][j] = i + j;
+	return 0;
+}
+
+
+
+int solucionOptimizada() {
+	int i, j;
+	int **m;
+	m = malloc(X * sizeof(int*));
+	for (i = 0; i < X; i++)
+		m[i] = malloc(Y * sizeof(int));
+	for(j = 0; j < Y; j++)
+		for(i = 0; i < X; i++)
+			m[j][i] = i + j;
+	return 0;
+}
+
+int main(int argc, char* argv[]){
+	if(argc<2)	return 1;
+	printf("caracter ingresado %s", argv[1]);
+	if(*argv[1]=='a'){
+		solucionInicial();
+	}else if(*argv[1]=='b'){
+		solucionOptimizada();
+	}
 	return 0;
 }
